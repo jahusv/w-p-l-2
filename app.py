03656bbd1490bb5d9ee5a6bcdd205d92e038426c
jahusv <jahusv@gmail.com>
2023-09-13 16:41:54 +0700

    Вывод первой страницы

--- app.py
+++ app.py
@@ -3,4 +3,25 @@ app = Flask (__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+    <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Панчук Анастасия Андреевна, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+        <h1>
+            web-сервер на flask
+        </h1>
+        <footer>
+            &copy; Панчук Анастасия Андреевна, ФБИ-13, Курс 3, 2023
+        </footer>
+    </body>
+</html>
+    """
