304823e266cccba48a67889f72cb198ec638bcca
jahusv <jahusv@gmail.com>
2023-09-13 18:02:42 +0700

    Статические файлы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
 app = Flask (__name__)
 
 @app.route("/")
@@ -7,11 +7,7 @@ def start():
     return redirect ("/menu", code=302)
 @app.route("/menu")
 def menu():
-    return """ 
-    
-    """
-    
-    """
+    return """  
     <!DOCTYPE html>
 <html>
     <head>
@@ -65,3 +61,17 @@ def lab1():
     </body>
 </html>
     """
+@app.route('/lab1/oak')
+def oak ():
+    return '''
+    <!doctype html>
+    <html>
+        <head> 
+            <link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+        </head>
+        <body>
+            <h1> Дуб </h1>
+            <img src="''' + url_for('static', filename='oak.jpg') + '''">
+        </body>
+    </html>
+    '''
