e7e1ba852934d7c95312ced83e8bfe7c65883e68
Артем Тарбанаков <ezdramiller@gmail.com>
2023-09-27 16:59:59 +0700

    Имя студента

--- app.py
+++ app.py
@@ -144,4 +144,5 @@ def pear ():
     
 @app.route('/lab2/example')
 def example():
-    return render_template ("example.html")
+    name = 'Анастасия Панчук'
+    return render_template ("example.html", name = name)
