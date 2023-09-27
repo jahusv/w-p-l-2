8aabed2f6c5da2ca1a32727d8e40d5adf8c8cf61
Артем Тарбанаков <ezdramiller@gmail.com>
2023-09-27 17:58:03 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -171,3 +171,7 @@ def example():
 
     return render_template ("example.html", name = name, 
             numCour = numCour, group = group, labNum = labNum, fruits = fruits, books = books)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
