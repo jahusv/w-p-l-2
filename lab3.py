12e9b3c3983482595e9a7afdc7acfb2a58547023
Анастасия Панчук <jahusv@gmail.com>
2023-10-24 19:08:14 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -1,6 +1,50 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, request
 lab3 = Blueprint('lab3', __name__)
+
 
 @lab3.route('/lab3/')
 def lab():
     return render_template('lab3.html')
+
+
+@lab3.route('/lab3/form1')
+def form1():
+    errors = {}
+    user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+    age = request.args.get('age')   
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+    sex = request.args.get('sex')
+
+    return render_template ('form1.html', user=user, age=age, sex=sex errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+    
+    if request.args.get('sugar') == 'on':
+        price += 10
+    if request.args.get('milk') == 'on':
+        price += 30
+
+    return render_template ('pay.html', price=price)
+
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template ('success.html')
