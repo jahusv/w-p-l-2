abaffb60bc585e6e032b7d2cb9d5797abd7eddf2
Анастасия Панчук <jahusv@gmail.com>
2023-10-28 20:22:04 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -37,7 +37,31 @@ def success():
     return render_template('/lab4/password_success')    
     
 
+@lab4.route('/lab4/refrigerator/', methods=['GET', 'POST'])
+def refrigerator():
+    message = ''
+    temperature = request.form.get('temperature')
+    if not temperature:
+        message = 'Ошибка: не задана температура'
+    else:
+            temperature = int(temperature)
+            if temperature < -12:
+                message = 'Не удалось установить температуру — слишком низкое значение'
+            elif -12 <= temperature <= -9:
+                message = f'Установлена температура: {temperature}°С\n❄❄❄'
+            elif -8 <= temperature <= -5:
+                message = f'Установлена температура: {temperature}°С\n❄❄'
+            elif -4 <= temperature <= -1:
+                message = f'Установлена температура: {temperature}°С\n❄'
+            elif temperature > -1:
+                message = 'Не удалось установить температуру — слишком высокое значение'
+            return render_template('temperature.html', message=message)
+    return render_template('refrigerator.html')
 
+
+@lab4.route('/lab4/temperature/')
+def temperature():
+    return render_template('temperature.html')
 
 
 
