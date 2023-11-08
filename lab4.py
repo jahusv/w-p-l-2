7ee22fd1ea451d22fb9e3d0ce25739f793ec01b1
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-11-08 16:02:12 +0700

    Лабораторная работа 5

--- lab4.py
+++ lab4.py
@@ -12,20 +12,24 @@ def login():
     username = request.form.get('username', '')
     password = request.form.get('password', '')
     if not username:        
-        error = ''
-    else: 
-        if not username:        
             error = 'Введите логин'
-        elif username != 'alex' and password != '123':
+            return render_template('login.html', error=error, username=username, password=password)
+    else: 
+        if username != 'alex' and password != '123':
             error = 'Неверный логин и/или пароль' 
+            return render_template('login.html', error=error, username=username, password=password)
         elif username != 'alex' and password == '123':
             error = 'Неверный логин'
+            return render_template('login.html', error=error, username=username, password=password)
         elif password != '123' and username == 'alex':
             error = 'Неверный пароль'
+            return render_template('login.html', error=error, username=username, password=password)
         elif not password:
             error = 'Введите пароль'
+            return render_template('login.html', error=error, username=username, password=password)
         elif not username and not password:
             error = 'Введите пароль и логин'
+            return render_template('login.html', error=error, username=username, password=password)
         else:
             return render_template ('password_success.html', error=error, 
                                         username=username, password=password)    
